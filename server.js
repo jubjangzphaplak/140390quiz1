835e67c81b233b8f8cd7e3bce3db4626dec93ffa
jubjang <jubjangphaplak40@gmail.com>
2018-09-17 17:46:58 +0700

    dd

--- server.js
+++ server.js
@@ -1,22 +1,48 @@
 
 var express = require('express');
-var app = express()
-
-
-
-app.set('view engine', 'ejs');
-app.get('/', function(req, res) {
-    res.render('pages/home');
-});
-app.get('/about', function(req, res) {
-    res.render('pages/students');
-});
-app.get('/about', function(req, res) {
-    res.render('pages/subjects');
-});
-
-
-
-
-console.log('App is running at http://localhost:8082');
-app.listen(8082);
+var app = express();
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+    host     : 'www.db4free.net',
+    user     : 's140390',
+    password : 'abc123**',
+    database : 'db140390'
+  });
+  connection.connect();
+  app.set('view engine', 'ejs');
+  //home
+  app.get('/', function (req, res) {
+      res.render('pages/home');
+  });
+  //all list of student
+  app.get('/students', function (req, res) {
+      
+      var sql ='select * from students';
+  
+      connection.query(sql,function (err, rows, fields){
+          if (err) throw err
+          res.render('pages/students',{students:rows});
+         
+      console.log('The student is: ', rows[0].students)
+      
+  })
+  });
+  
+  //all list of subjects
+  app.get('/subjects', function (req, res) {
+      
+      var sql ='select * from subjects';
+      connection.query(sql,function (err, rows, fields){
+          if (err) throw err
+          res.render('pages/subjects',{subjects:rows});
+      console.log('The subject is: ', rows[0].subjects);
+  
+  })
+  });
+  
+  app.get('/index', function (req, res) {
+      res.render('pages/home');
+  });
+  console.log('App is running at http://localhost:8082');
+  
+  app.listen(8082);
