468d288e8863479d16faf2a9238f658d6e259875
jubjang <jubjangphaplak40@gmail.com>
2018-09-17 17:41:25 +0700

    db

--- server.js
+++ server.js
@@ -1,23 +1,16 @@
-// server.js
-// load the things we need
+
 var express = require('express');
-
 var app = express()
-// set the view engine to ejs
+
+
+
 app.set('view engine', 'ejs');
-
-// use res.render to load up an ejs view file
-
-// index page 
 app.get('/', function(req, res) {
     res.render('pages/home');
 });
-
-// about page 
 app.get('/about', function(req, res) {
     res.render('pages/students');
 });
-
 app.get('/about', function(req, res) {
     res.render('pages/subjects');
 });
@@ -25,8 +18,5 @@ app.get('/about', function(req, res) {
 
 
 
-
 console.log('App is running at http://localhost:8082');
-
-
 app.listen(8082);
