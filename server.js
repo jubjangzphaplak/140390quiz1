1910853dd5541882b160c47bc33e63104033552e
jubjang <jubjangphaplak40@gmail.com>
2018-09-17 16:06:16 +0700

    code1

--- server.js
+++ server.js
@@ -1,70 +1,22 @@
+// server.js
+// load the things we need
 var express = require('express');
-
-var pgp = require('pg-promise')();
-var db = pgp('postgres://nkwnjxuiidwrns:b72b4de42f726173c9acee8a85dd10ed1c8dc1a2ab7402a6feebbbccb8b14f85@ec2-54-163-245-44.compute-1.amazonaws.com:5432/d34ii1v5fr4h1e?ssl=true');
 var app = express();
 
-//app.use(express.static('static'));
+// set the view engine to ejs
 app.set('view engine', 'ejs');
 
-app.get('/', function (req, res) {
-    res.render('pages/index')
+// use res.render to load up an ejs view file
+
+// index page 
+app.get('/', function(req, res) {
+    res.render('pages/index');
 });
 
-app.get('/about', function (req, res) {
-
-    var name = 'Jubjang Phaplak';
-    var hobbies = ['Music', 'Movie', 'WebDesign'];
-    var bdate = '09/06/1997'
-    res.render('pages/about', { fullname: name, hobbies: hobbies, bdate: bdate });
+// about page 
+app.get('/about', function(req, res) {
+    res.render('pages/about');
 });
 
-app.get('/products', function (req, res) {
-    var id = req.param('id');
-    var sql = 'select *from products';
-    if (id) {
-        sql += 'where id=' + id;
-    }
-    db.any(sql)
-        .then(function (data) {
-            console.log('DATA:' + data);
-            res.render('pages/products', { products: data })
-
-
-        })
-        .catch(function (error) {
-
-            console.log('ERROR:' + error);
-
-
-
-
-        })
-})
-    
-
-app.get('/users', function (req, res) {
-
-
-    db.any('select * from users', )
-        .then(function (data) {
-            console.log('DATA:' + data);
-            res.render('pages/users', { users: data })
-
-
-        })
-        .catch(function (error) {
-
-            console.log('ERROR:' + error);
-
-        }
-
-        )
-}
-);
-
-console.log('App is running at http://localhost:8000');
-
-
-app.listen(8000);
-
+app.listen(8080);
+console.log('8080 is the magic port');
